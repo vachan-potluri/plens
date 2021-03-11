38f103e4a3c3010165f1fb27feab4fc7cd7900d5
vachan-potluri <vachanpotluri1997@gmail.com>
2021-03-11 11:06:43 +0530

    Initial commit

---
+++ dgsem/dtype_aliases.h
@@ -0,0 +1,16 @@
+/**
+ * @file dtype_aliases.h
+ * @brief Defines different aliases for data types that are used commonly.
+ */
+
+#ifndef DTYPE_ALIASES_H
+#define DTYPE_ALIASES_H
+
+// A size data type for 'p'lens. Generally used for cell index or global dof index
+using psize = std::size_t;
+
+// Generally used for face id w.r.t, cell and dof id w.r.t. cell (i.e. local dof id)
+using usi = unsigned short int;
+
+#endif
+
