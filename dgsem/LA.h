e3c1615535328d542aef67a725221787a0f3b768
vachan-potluri <vachanpotluri1997@gmail.com>
2021-03-11 11:55:12 +0530

    Initial commit

---
+++ dgsem/LA.h
@@ -0,0 +1,17 @@
+/**
+ * @file LA.h
+ * @brief Defines the linear algebra namespace
+ */
+
+#include <deal.II/lac/generic_linear_algebra.h>
+
+#ifndef LA_H
+#define LA_H
+
+namespace LA
+{
+    using namespace ::LinearAlgebraPETSc;
+} // linear algebra namespace
+
+#endif
+
