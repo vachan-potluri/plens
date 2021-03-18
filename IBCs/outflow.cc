40dbd9b80f16e5140b275887f7bc927de797488f
vachan-potluri <vachanpotluri1997@gmail.com>
2021-03-18 10:44:00 +0530

    Initial commit

---
+++ IBCs/outflow.cc
@@ -0,0 +1,46 @@
+/**
+ * @file outflow.cc
+ * @brief Outflow BC
+ */
+
+#include "outflow.h"
+
+using namespace BCs;
+
+/**
+ * @brief Sets the ghost conservative state to inner conservative state obtained using
+ * BC::get_state().
+ *
+ * @note @p normal is unused
+ */
+void Outflow::get_ghost_stage1(
+    const LocalDoFData &ldd,
+    const Tensor<1,dim> &normal,
+    State &cons_gh
+) const
+{
+    State cons_in;
+    get_state(ldd, cons_in);
+    cons_gh = cons_in;
+}
+
+
+
+/**
+ * @brief Sets the ghost cavars to inner cavars obtained using BC::get_cavars().
+ *
+ * @note @p normal is unused
+ */
+void Outflow::get_ghost_stage3(
+    const LocalDoFData &ldd,
+    const Tensor<1,dim> &normal,
+    CAvars &cav_gh
+) const
+{
+    State cons;
+    Avars av;
+    CAvars cav_in(&cons, &av);
+    get_cavars(ldd, cav_in);
+    cav_gh = cav_in;
+}
+
