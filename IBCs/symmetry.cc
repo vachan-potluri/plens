0eae8e6e2708a70c114719877469262b4bef9d21
vachan-potluri <vachanpotluri1997@gmail.com>
2021-03-22 11:43:11 +0530

    Modify algo for stage 2 getter

--- IBCs/symmetry.cc
+++ IBCs/symmetry.cc
@@ -35,7 +35,9 @@ void Symmetry::get_ghost_stage1(
 
 
 /**
- * Sets the ghost state according to the algo described in class documentation
+ * Sets the ghost state according to the algo described in class documentation. Here, the normal
+ * velocity is reversed and tangential velocity is retained. Which means the kinetic energy doesn't
+ * change. Hence, only components 1-3 of the conservative state need to be changed.
  *
  * @pre @p normal has to be a unit vector
  */
@@ -47,14 +49,13 @@ void Symmetry::get_ghost_stage2(
 {
     State cons_in;
     get_state(ldd, cons_in);
+    cons_gh[0] = cons_in[0];
+    cons_gh[4] = cons_in[4];
     
-    Tensor<1,dim> vel_in, vel_gh;
-    for(int d=0; d<dim; d++) vel_in[d] = cons_in[1+d]/cons_in[0];
-    double normal_vel = scalar_product(vel_in, normal); // vel_in dot normal
-    for(int d=0; d<dim; d++) vel_gh[d] = vel_in[d] - 2*normal_vel*normal[d];
-    
-    double p_in = ns_ptr_->get_p(cons_in);
-    ns_ptr_->prim_to_cons(cons_in[0], vel_gh, p_in, cons_gh); // sets cons_gh
+    Tensor<1,dim> smom_in, smom_gh; // specific momentum
+    for(int d=0; d<dim; d++) smom_in[d] = cons_in[1+d];
+    double normal_smom = scalar_product(smom_in, normal); // smom_in dot normal
+    for(int d=0; d<dim; d++) smom_gh[d] = smom_in[d] - 2*normal_smom*normal[d];
 }
 
 
@@ -103,7 +104,7 @@ void Symmetry::test()
     
     {
         t.new_block("testing ghost getters");
-        Tensor<1,dim> normal; // immaterial
+        Tensor<1,dim> normal({0,0,1});
         LocalDoFData ldd(1, 1, 3); // cell id, face id, face dof id
         
         // modify bctd.g_cvars to get subsonic/supersonic state
@@ -112,8 +113,9 @@ void Symmetry::test()
         psize gdof_id = bc_p->get_global_dof_id(ldd);
         for(cvar var: cvar_list) bctd.g_cvars[var][gdof_id] = cons[var];
         
-        Avars av;
-        CAvars cav(&cons, &av);
+        Avars avv;
+        CAvars cav(&cons, &avv);
+        utilities::print_avars(cav.get_avars());
         
         for(int i=0; i<3; i++){
             std::cout << "Stage " << i << "\n";
