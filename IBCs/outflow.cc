1db1c4faf4cfb7063bcc8f7577f074820f02b6da
vachan-potluri <vachanpotluri1997@gmail.com>
2021-03-31 10:38:15 +0530

    Make getter algo simpler

--- IBCs/outflow.cc
+++ IBCs/outflow.cc
@@ -19,9 +19,7 @@ void Outflow::get_ghost_stage1(
     State &cons_gh
 ) const
 {
-    State cons_in;
-    get_state(ldd, cons_in);
-    cons_gh = cons_in;
+    get_state(ldd, cons_gh);
 }
 
 
@@ -65,11 +63,7 @@ void Outflow::get_ghost_stage3(
     CAvars &cav_gh
 ) const
 {
-    State cons;
-    Avars av;
-    CAvars cav_in(&cons, &av);
-    get_cavars(ldd, cav_in);
-    cav_gh = cav_in;
+    get_cavars(ldd, cav_gh);
 }
 
 
