a9a143e3bc084f3d41f043c662d36af005edb414
vachan-potluri <vachanpotluri1997@gmail.com>
2021-04-07 09:44:42 +0530

    Initial commit

---
+++ dgsem/plens_test.cc
@@ -0,0 +1,9 @@
+/**
+ * @file plens_test.cc
+ * @brief A class to test PLENS class.
+ */
+
+#include "plens_test.h"
+
+void plens_test::read_mesh_test()
+{}
---
+++ dgsem/plens_test.h
@@ -0,0 +1,22 @@
+/**
+ * @file plens_test.h
+ * @brief A class to test PLENS class.
+ */
+
+#ifndef PLENS_TEST_H
+#define PLENS_TEST_H
+
+#include "plens.h"
+
+using namespace dealii;
+/**
+ * @class plens_test
+ * @brief A class to test PLENS class.
+ */
+class plens_test
+{
+    public:
+    void read_mesh_test() const;
+};
+
+#endif
