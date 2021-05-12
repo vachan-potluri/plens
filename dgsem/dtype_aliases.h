2197e6e80613b634568126a0e0bd30b2ba5d889b
Vachan Potluri <vachanpotluri1997@gmail.com>
2021-05-12 12:14:20 +0530

    Change alias psize to types::global_dof_index

--- dgsem/dtype_aliases.h
+++ dgsem/dtype_aliases.h
@@ -6,8 +6,11 @@
 #ifndef DTYPE_ALIASES_H
 #define DTYPE_ALIASES_H
 
-// A size data type for 'p'lens. Generally used for cell index or global dof index
-using psize = std::size_t;
+#include <deal.II/base/types.h>
+
+// A size data type for 'p'lens. Generally used for cell index or global dof index. See
+// https://www.dealii.org/current/doxygen/deal.II/GlobalDoFIndex.html
+using psize = dealii::types::global_dof_index;
 
 // Generally used for face id w.r.t, cell and dof id w.r.t. cell/face
 using usi = unsigned short int;
