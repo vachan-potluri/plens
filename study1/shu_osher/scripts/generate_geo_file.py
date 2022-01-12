044e3afc1ee8b56b0e8ff2b77c3af1d4cad4fa97
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-01-12 11:35:30 +0530

    Modify argument to takes number of cells in y and z directions

--- study1/shu_osher/scripts/generate_geo_file.py
+++ study1/shu_osher/scripts/generate_geo_file.py
@@ -5,7 +5,12 @@ import argparse
 parser = argparse.ArgumentParser(
     description = "A script to generate gmsh file to generate mesh for Shu-Osher problem."
 )
-parser.add_argument("n_cells", type=int, help="Number of cells in the mesh")
+parser.add_argument(
+    "n_cells",
+    type=int,
+    nargs=3,
+    help="Number of cells in the 3 directions in the mesh"
+)
 parser.add_argument("dest", help="Location where the gmsh file should be written")
 parser.add_argument(
     "-f",
@@ -28,7 +33,7 @@ Line(3) = {{3,4}};
 Line(4) = {{4,1}};
 
 Transfinite Line{{1,3}} = {};
-Transfinite Line{{2,4}} = 2;
+Transfinite Line{{2,4}} = {};
 
 Line Loop(1) = {{1,2,3,4}};
 Plane Surface(1) = {{1}};
@@ -36,7 +41,7 @@ Transfinite Surface{{1}};
 Recombine Surface{{1}};
 
 out[] = Extrude {{0, 0, 0.05}}{{
-    Surface{{1}}; Layers{{1}}; Recombine;
+    Surface{{1}}; Layers{{ {} }}; Recombine;
 }};
 
 Physical Volume("volume", 1) = {{out[1]}};
@@ -44,7 +49,7 @@ Physical Surface("left", 1) = {{out[5]}}; // left
 Physical Surface("right", 2) = {{out[3]}}; // right
 Physical Surface("symmetry", 3) = {{out[2], out[4], 1, out[0]}}; // top and bottom, front and back
 
-""".format(args.n_cells+1)
+""".format(args.n_cells[0]+1, args.n_cells[1]+1, args.n_cells[2])
 
 filename = args.filename
 if filename[-4:] != ".geo":
