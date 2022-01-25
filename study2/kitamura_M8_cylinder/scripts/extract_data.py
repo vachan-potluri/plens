5db8d87a6d5822b61604567760ba0bff04731cbe
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-01-25 13:47:21 +0530

    Equip the extractor to use argparse

--- study2/kitamura_M8_cylinder/scripts/extract_data.py
+++ study2/kitamura_M8_cylinder/scripts/extract_data.py
@@ -1,11 +1,43 @@
+# A script to extract line data from pvtu files for Kitamura's Mach 8 flow over cylinder
 
+import argparse
 from paraview.simple import *
 
-# find source
-dummypvd = XMLPartitionedUnstructuredGridReader(FileName='dummy.pvd')
+parser = argparse.ArgumentParser(
+    description = "A script to extract line data from pvtu files for Kitamura's case. This script "
+        + "has to be executed using 'pvpython'."
+)
+parser.add_argument("res_dir", help="Result directory (absolute or relative).")
+parser.add_argument("counter", help="Output counter.", type=int)
+parser.add_argument(
+    "-b",
+    "--base_output_filename",
+    help="Base output file name. Default: 'output'.",
+    default="output",
+    action="store"
+)
+parser.add_argument(
+    "-r",
+    "--resolution",
+    help="Resolution of line source. Default: 100.",
+    type=int,
+    default=100,
+    action="store"
+)
+args = parser.parse_args()
+
+res_dir = args.res_dir
+if res_dir[-1] != "/":
+    res_dir += "/"
+counter = args.counter
+full_data_filename = "{}{}_{:06d}.pvtu".format(res_dir, args.base_output_filename, counter)
+result = XMLPartitionedUnstructuredGridReader(
+    FileName=full_data_filename
+)
+print("Read file {}".format(full_data_filename))
 
 # create a new 'Slice'
-slice1 = Slice(registrationName='Slice1', Input=dummypvd)
+slice1 = Slice(registrationName='Slice1', Input=result)
 slice1.SliceType = 'Plane'
 slice1.HyperTreeGridSlicer = 'Plane'
 slice1.SliceOffsetValues = [0.0]
@@ -23,6 +55,7 @@ plotOnIntersectionCurves1.SliceType.Center = [0.0, 0.0, 0.01]
 plotOnIntersectionCurves1.SliceType.Radius = 0.020000001
 
 # save data
+temp_filename = '{}surface_data_{}.csv'.format(res_dir, counter)
 SaveData(
     'surface_data.csv',
     proxy=plotOnIntersectionCurves1,
