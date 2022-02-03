8682e1910dc0450571cd9b81f7311b55fe370508
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-02-03 09:50:43 +0530

    Small change

--- study1/riemann_1d/scripts/plot_group.py
+++ study1/riemann_1d/scripts/plot_group.py
@@ -83,22 +83,22 @@ plt.rcParams["mathtext.fontset"] = "dejavuserif"
 #     "../test1-3/run/dof800_12_12_N1_hllc/result/comparison_data.csv",
 #     "../test1-3/run/dof800_12_12_N2_hllc/result/comparison_data.csv",
 #     "../test1-3/run/dof800_12_12_N3_hllc/result/comparison_data.csv",
-#     "../test1-3/run/dof800_12_12_N5_hllc/result/comparison_data.csv"
-# ]
-# data_files = [
-#     "../test1-4/run/dof800_12_12_N1_hllc/result/comparison_data.csv",
-#     "../test1-4/run/dof800_12_12_N2_hllc/result/comparison_data.csv",
-#     "../test1-4/run/dof800_12_12_N3_hllc/result/comparison_data.csv",
-#     "../test1-4/run/dof800_12_12_N5_hllc/result/comparison_data.csv"
+#     "../test1-3/run/dof800_12_12_N5_hllc/result1/comparison_data.csv"
 # ]
 data_files = [
-    "../test1-5/run/dof800_12_12_N1_rusanov/result/comparison_data.csv",
-    "../test1-5/run/dof800_12_12_N2_rusanov/result/comparison_data.csv",
-    "../test1-5/run/dof800_12_12_N3_rusanov/result/comparison_data.csv",
-    "../test1-5/run/dof800_12_12_N5_rusanov/result1/comparison_data.csv"
+    "../test1-4/run/dof800_12_12_N1_hllc/result/comparison_data.csv",
+    "../test1-4/run/dof800_12_12_N2_hllc/result/comparison_data.csv",
+    "../test1-4/run/dof800_12_12_N3_hllc/result/comparison_data.csv",
+    "../test1-4/run/dof800_12_12_N5_hllc/result1/comparison_data.csv"
 ]
+# data_files = [
+#     "../test1-5/run/dof800_12_12_N1_rusanov/result/comparison_data.csv",
+#     "../test1-5/run/dof800_12_12_N2_rusanov/result/comparison_data.csv",
+#     "../test1-5/run/dof800_12_12_N3_rusanov/result/comparison_data.csv",
+#     "../test1-5/run/dof800_12_12_N5_rusanov/result1/comparison_data.csv"
+# ]
 titles = [r"$N=1$", r"$N=2$", r"$N=3$", r"$N=5$"]
-figtitle = r"test1-5 $\rho$ vs $x$"
+figtitle = r"test1-4 $\rho$ vs $x$"
 
 n_plots = len(data_files)
 fig, axes = plt.subplots(2,2)
