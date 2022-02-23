3b08b8289ba9b30302f9f02877983b5e24726dd1
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-02-23 16:04:16 +0530

    More data and updates to plotting script

--- study2/hung_M14_wedge18/scripts/plot.py
+++ study2/hung_M14_wedge18/scripts/plot.py
@@ -18,6 +18,9 @@ plt.rcParams["font.size"] = 10
 p_inf = 9.99473
 rho_inf = 0.0004827
 u_inf = 2401.824
+T_inf = 72.22
+T_w = 297.22
+cp_air = 1005
 L = 0.438912
 wedge_angle = 18*np.pi/180
 
@@ -41,8 +44,10 @@ ramp_data = np.genfromtxt(
 
 holden_cp = np.genfromtxt("../data/holden_cp.csv", delimiter=",")
 hung_cp = np.genfromtxt("../data/hung_cp.csv", delimiter=",")
-# harvey_St = np.genfromtxt("../data/St_harvey.csv", delimiter=",")
-# candler_St = np.genfromtxt("../data/St_gnoffo.csv", delimiter=",")
+holden_ch = np.genfromtxt("../data/holden_ch.csv", delimiter=",")
+hung_ch = np.genfromtxt("../data/hung_ch.csv", delimiter=",")
+holden_cf = np.genfromtxt("../data/holden_cf.csv", delimiter=",")
+hung_cf = np.genfromtxt("../data/hung_cf.csv", delimiter=",")
 
 x_plate = plate_data["Points0"]/L
 x_ramp = ramp_data["Points0"]/L
@@ -53,15 +58,19 @@ cp_ramp = 2*(ramp_data["p"])/(rho_inf*u_inf**2)
 cp = np.concatenate([cp_plate, cp_ramp])
 cp_mask = np.isfinite(cp)
 
-# St_plate = -2*plate_data["qy"]/(rho_inf*u_inf**3)
-# St_ramp = -2*(ramp_data["qy"]*np.cos(np.pi/6) - ramp_data["qx"]*np.sin(np.pi/6))/(rho_inf*u_inf**3)
-# St = np.concatenate([St_plate, St_ramp])
-# St_mask = np.isfinite(St)
+ch_plate = -plate_data["qy"]/(rho_inf*u_inf*(0.5*u_inf**2 + cp_air*(T_inf-T_w)))
+ch_ramp = -(ramp_data["qy"]/np.cos(wedge_angle))/(rho_inf*u_inf*(0.5*u_inf**2 + cp_air*(T_inf-T_w)))
+ch = np.concatenate([ch_plate, ch_ramp])
+ch_mask = np.isfinite(ch)
 
-# txy_plate = plate_data["txy"]
-# txy_ramp = ramp_data["txy"]
-# txy = np.concatenate([txy_plate, txy_ramp])
-# txy_mask = np.isfinite(txy)
+cf_plate = 2*plate_data["txy"]/(rho_inf*u_inf**2)
+cf_ramp = 2*(
+    ramp_data["txy"]
+    # ramp_data["txy"]*np.cos(wedge_angle) + ramp_data["tyy"]*np.sin(wedge_angle) +
+    # ramp_data["txx"]*np.cos(wedge_angle) + ramp_data["txy"]*np.sin(wedge_angle)
+    )/(rho_inf*u_inf**2)
+cf = np.concatenate([cf_plate, cf_ramp])
+cf_mask = np.isfinite(cf)
 
 fig, ax = plt.subplots(1,1)
 ax.plot(holden_cp[:,0], 2*holden_cp[:,1], "bo", markersize=4, label="Holden \& Moselle\n(1970, experiment)")
@@ -69,7 +78,8 @@ ax.plot(hung_cp[:,0], 2*hung_cp[:,1], "gx", markersize=4, label="Hung \& MacCorm
 ax.plot(s_wall[cp_mask], cp[cp_mask], "r-", label="PLENS")
 ax.set_xlabel(r"$s_{\textrm{wall}}/L$")
 ax.set_ylabel(r"$\displaystyle\frac{p}{\frac{1}{2}\rho_\infty u_\infty^2}$", rotation=0, labelpad=20)
-ax.grid()
+ax.set_yscale("log")
+ax.grid(which="both")
 ax.legend(loc="best")
 fig.tight_layout()
 for fmt in ["png", "pdf"]:
@@ -78,44 +88,44 @@ for fmt in ["png", "pdf"]:
     print("Written file {}".format(full_figname))
 plt.show()
 
-# del fig, ax
+fig, ax = plt.subplots(1,1)
+ax.plot(holden_ch[:,0], holden_ch[:,1], "bo", markersize=4, label="Holden \& Moselle\n(1970, experiment)")
+ax.plot(hung_ch[:,0], hung_ch[:,1], "gx", markersize=4, label="Hung \& MacCormack\n(1976, simulation)")
+ax.plot(s_wall[ch_mask], ch[ch_mask], "r-", label="PLENS")
+ax.set_xlabel(r"$s_{\textrm{wall}}/L$")
+ax.set_ylabel(
+    # r"$\displaystyle\frac{-q^{\prime\prime}_y \sec \theta_i}"
+    # r"{\rho_\infty u_\infty \left( h_\infty - h_w + \frac{u_\infty^2}{2} \right)}$",
+    # rotation=0,
+    # labelpad=20
+    r"$c_H$"
+)
+ax.set_yscale("log")
+ax.grid(which="both")
+ax.legend(loc="best")
+fig.tight_layout()
+for fmt in ["png", "pdf"]:
+    full_figname = "{}ch_comparison_{}.{}".format(res_dir, counter, fmt)
+    fig.savefig(full_figname, format=fmt)
+    print("Written file {}".format(full_figname))
+plt.show()
+
 # fig, ax = plt.subplots(1,1)
-# ax.plot(harvey_St[:,0], harvey_St[:,1], "bo", markersize=4, label="Harvey et al (2001)\nexperiment")
-# ax.plot(candler_St[:,0], candler_St[:,1], "gx", markersize=4, label="Gnoffo (2001)\nsimulation")
-# ax.plot(x[St_mask], St[St_mask], "r-", label="PLENS")
-# ax.set_xlabel(r"$x/L$")
-# ax.set_ylabel(r"$\displaystyle\frac{2q^{\prime\prime}_w}{\rho_\infty u_\infty^3}$", rotation=0, labelpad=20)
+# ax.plot(holden_cf[:,0], holden_cf[:,1], "bo", markersize=4, label="Holden \& Moselle\n(1970, experiment)")
+# ax.plot(hung_cf[:,0], hung_cf[:,1], "gx", markersize=4, label="Hung \& MacCormack\n(1976, simulation)")
+# ax.plot(s_wall[cf_mask], cf[cf_mask], "r-", label="PLENS")
+# ax.set_xlabel(r"$s_{\textrm{wall}}/L$")
+# ax.set_ylabel(
+#     # r"$\frac{2\tau_{xy}}{\rho_\infty u_\infty^2}$"
+#     # rotation=0,
+#     # labelpad=20
+#     r"$c_f$"
+# )
 # ax.grid()
 # ax.legend(loc="best")
 # fig.tight_layout()
 # for fmt in ["png", "pdf"]:
-#     full_figname = "{}St_comparison_{}.{}".format(res_dir, counter, fmt)
+#     full_figname = "{}cf_comparison_{}.{}".format(res_dir, counter, fmt)
 #     fig.savefig(full_figname, format=fmt)
 #     print("Written file {}".format(full_figname))
 # plt.show()
-
-
-
-"""
-#del fig, ax
-fig, ax = plt.subplots(1,1)
-ax.plot(x[txy_mask], txy[txy_mask], "r-", label="PLENS")
-# Gnoffo's results on unadaptive 272 by 96 grid
-x_sep_gnoffo = 0.52
-x_att_gnoffo = 1.31
-txy_max = np.max(txy[txy_mask])
-txy_min = np.min(txy[txy_mask])
-txy_range = np.array([txy_min, -txy_min])
-ax.plot(x_sep_gnoffo*np.ones(2), txy_range, "g--")
-ax.plot(x_att_gnoffo*np.ones(2), txy_range, "g--", label="Gnoffo (2001) separation\n& attachment locations")
-ax.legend()
-ax.minorticks_on()
-ax.grid(which="major", linestyle='-', linewidth='0.5')
-ax.grid(which="minor", linestyle='-', linewidth='0.25')
-ax.set_xlabel(r"$x/L$")
-ax.set_ylabel(r"$\tau_{xy}$ [Pa]")
-fig.tight_layout()
-fig.savefig("{}txy_comparison_{}.png".format(res_dir, counter), format="png")
-fig.savefig("{}txy_comparison_{}.pdf".format(res_dir, counter), format="pdf")
-plt.show()
-"""
