cc803321df1edb8f1f5d5f354863c8ace02c4a6a
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-04-21 09:48:18 +0530

    Add a small script to plot `*.ss_error` files

---
+++ utilities/plot_ss_error.py
@@ -0,0 +1,29 @@
+# A small script to plot the error data from `./output.ss_error`
+# The file must be available in the working directory
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Palatino"],
+    "font.size": 12,
+    "axes.formatter.limits": [-2,2]
+})
+
+data = np.genfromtxt("output.ss_error")
+time = data[:,1]
+ss_error = data[:,2]
+mask = (ss_error != 1) # indicates simulation start and/or restart
+fig, ax = plt.subplots(1,1)
+ax.plot(time[mask], ss_error[mask], "b-")
+ax.set_yscale("log")
+ax.set_xlabel(r"$t$ [sec]")
+ax.set_ylabel("Residual")
+ax.grid()
+fig.tight_layout()
+plt.show()
+for fmt in ["png", "pdf"]:
+    figname = "ss_error_plot.{}".format(fmt)
+    fig.savefig(figname)
+    print("Saved figure {}".format(figname))
