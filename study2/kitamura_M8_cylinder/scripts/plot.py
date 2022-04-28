3a7aee709eda4c30e6ab9251382d25449c288a62
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-04-28 15:30:48 +0530

    Changes to plotting script and some data for Kitamura's cylinder case

--- study2/kitamura_M8_cylinder/scripts/plot.py
+++ study2/kitamura_M8_cylinder/scripts/plot.py
@@ -1,9 +1,13 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["mathtext.fontset"] = "dejavuserif"
-plt.rcParams["font.size"] = 10
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Palatino"],
+    "font.size": 12,
+    "axes.formatter.limits": [-2,2]
+})
 
 parser = argparse.ArgumentParser(
     description = "A script to plot extracted data for Kitamura's case."
@@ -20,25 +24,40 @@ counter = args.counter
 surface_data = np.genfromtxt(
     "{}surface_data_{}.csv".format(res_dir, counter), delimiter=",", names=True
 )
+kitamura_p_data = np.genfromtxt("../data/kitamura_p_ausmpw.csv", delimiter=",")
+kitamura_q_data = np.genfromtxt("../data/kitamura_q_ausmpw.csv", delimiter=",")
 
 r = 0.02
 p_inf = 370.7
 p0 = p_inf*84.9 # this ratio is given in Kitamura (2010)
+q_fr = 17.5e4 # given in Kitamura et al (2010): Fay & Riddells value
 p = surface_data["p"]
 theta = np.arcsin(surface_data["Points1"]/r)
 q = surface_data["qx"]*np.cos(theta) - surface_data["qy"]*np.sin(theta)
 
 fig, ax = plt.subplots(1,1)
-ax.plot(theta*180/np.pi, p/p0, "r-")
+ax.plot(kitamura_p_data[:,0], kitamura_p_data[:,1], "bo", label="Kitamura et al\n(2010) simulation")
+ax.plot(theta*180/np.pi, p/p0, "r-", label="PLENS")
+ax.legend()
 ax.grid()
 ax.set_xlabel(r"$\theta$ [degrees]")
-ax.set_ylabel(r"$\frac{p}{p_0}$")
+ax.set_ylabel(r"$\displaystyle\frac{p}{p_{10}}$", rotation=0, labelpad=20)
 plt.show()
+for fmt in ["png", "pdf"]:
+    fig_filename = "{}p_{}.{}".format(res_dir, counter, fmt)
+    fig.savefig(fig_filename, format=fmt)
+    print("Saved figure {}".format(fig_filename))
 del fig, ax
 
 fig, ax = plt.subplots(1,1)
-ax.plot(theta*180/np.pi, q, "r-")
+ax.plot(kitamura_q_data[:,0], kitamura_q_data[:,1], "bo", label="Kitamura et al\n(2010) simulation")
+ax.plot(theta*180/np.pi, q/q_fr, "r-", label="PLENS")
+ax.legend()
 ax.grid()
 ax.set_xlabel(r"$\theta$ [degrees]")
-ax.set_ylabel(r"$Heat flux [W/sqm]")
+ax.set_ylabel(r"$\displaystyle\frac{q^{''}}{q^{''}_{\textrm{FR}}}$", rotation=0, labelpad=20)
 plt.show()
+for fmt in ["png", "pdf"]:
+    fig_filename = "{}q_{}.{}".format(res_dir, counter, fmt)
+    fig.savefig(fig_filename, format=fmt)
+    print("Saved figure {}".format(fig_filename))
