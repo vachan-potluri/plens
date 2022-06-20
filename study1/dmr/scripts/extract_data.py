5ad1994f9076308ec235439b6a5e4e3a17155510
Vachan Potluri <vachanpotluri1997@gmail.com>
2022-06-20 15:12:50 +0530

    Extraction and plotting scripts for dmr case

---
+++ study1/dmr/scripts/extract_data.py
@@ -0,0 +1,93 @@
+# A script to extract line data from pvtu files for DMR case
+
+import argparse
+from paraview.simple import *
+
+parser = argparse.ArgumentParser(
+    description = "A script to extract line data from pvtu files for DMR case. This script "
+        + "has to be executed using 'pvpython'."
+)
+parser.add_argument("res_dir", help="Result directory (absolute or relative).")
+parser.add_argument("counter", help="Output counter.", type=int)
+parser.add_argument(
+    "-b",
+    "--base_output_filename",
+    help="Base output file name. Default: 'output'.",
+    default="output",
+    action="store"
+)
+parser.add_argument(
+    "-r",
+    "--resolution",
+    help="Resolution of line source. Default: 6400.",
+    type=int,
+    default=1000,
+    action="store"
+)
+parser.add_argument(
+    "-d",
+    "--base_data_filename",
+    help="Base data file name. Default: 'line_data'. Data is stored as csv file with 'counter' "
+        + "appended to the filename. The file is stored in 'res_dir'.",
+    default="line_data",
+    action="store"
+)
+args = parser.parse_args()
+
+res_dir = args.res_dir
+if res_dir[-1] != "/":
+    res_dir += "/"
+
+
+
+z_loc = 0.25 # location of the z-normal plane in which lines are sampled
+
+
+
+full_op_filename = res_dir + args.base_output_filename + "_{:06d}.pvtu".format(args.counter)
+print("Reading output file {}".format(full_op_filename))
+result = XMLPartitionedUnstructuredGridReader(FileName=full_op_filename)
+
+
+
+# create a new 'Plot Over Line'
+plotOverLine1 = PlotOverLine(
+    registrationName='PlotOverLine1',
+    Input=result,
+    Source='Line'
+)
+
+plotOverLine1.Source.Point1 = [2.5, 0, z_loc]
+plotOverLine1.Source.Point2 = [2.5, 0.5, z_loc]
+plotOverLine1.Source.Resolution = args.resolution
+
+# save data
+full_data_filename = res_dir + args.base_data_filename + "_x2.5.csv"
+SaveData(
+    full_data_filename,
+    proxy=plotOverLine1,
+    PointDataArrays=['Subdomain', 'T', 'alpha', 'arc_length', 'k', 'mu', 'p', 'qx', 'qy', 'qz', 'rho', 'rhoE', 'rhou', 'rhov', 'rhow', 'steady_state_error', 'txx', 'txy', 'txz', 'tyy', 'tyz', 'tzz', 'u', 'v', 'vtkValidPointMask', 'w']
+)
+print("Written data file {}".format(full_data_filename))
+
+
+
+# create a new 'Plot Over Line'
+plotOverLine2 = PlotOverLine(
+    registrationName='PlotOverLine2',
+    Input=result,
+    Source='Line'
+)
+
+plotOverLine2.Source.Point1 = [2, 0.3, z_loc]
+plotOverLine2.Source.Point2 = [3, 0.3, z_loc]
+plotOverLine2.Source.Resolution = args.resolution
+
+# save data
+full_data_filename = res_dir + args.base_data_filename + "_y0.3.csv"
+SaveData(
+    full_data_filename,
+    proxy=plotOverLine2,
+    PointDataArrays=['Subdomain', 'T', 'alpha', 'arc_length', 'k', 'mu', 'p', 'qx', 'qy', 'qz', 'rho', 'rhoE', 'rhou', 'rhov', 'rhow', 'steady_state_error', 'txx', 'txy', 'txz', 'tyy', 'tyz', 'tzz', 'u', 'v', 'vtkValidPointMask', 'w']
+)
+print("Written data file {}".format(full_data_filename))
---
+++ study1/dmr/scripts/plot_group.py
@@ -0,0 +1,67 @@
+# A script to do line plots of extracted data. This script can be executed from any location as
+# long as the `run_dir` and `res_dirs` variables are set correctly.
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.size": 14,
+    "axes.formatter.limits": [-2,2]
+})
+import pandas as pd
+
+# the common directory for all runs
+run_dir = "/home/vachan/Documents/Work/plens/study1/dmr/run"
+N_values = [1,2,3,5]
+flux = "chandrashekhar" # flux scheme used, generally this is a suffix to the case dir
+res_dir_name = "result" # name of result dir within case dir
+line_loc_suffixes = ["x2.5", "y0.3"] # the suffixes for the different number of lines sampled
+plot_var = "rho" # variable for plotting, use to index the data file
+yaxis_label = r"$\rho$"
+plot_dir = "/home/vachan/Documents/Work/plens/study1/dmr/plots" # loc for saving plots
+
+# set the data file names
+data_filenames = pd.DataFrame(data="", index=N_values, columns=line_loc_suffixes)
+for N in N_values:
+    res_dir = "{}/res_60_N{}_{}/{}/".format(run_dir, N, flux, res_dir_name)
+    for s in line_loc_suffixes:
+        data_filenames.loc[N, s] = res_dir+ "line_data_{}.csv".format(s)
+
+# set the x-axis variable for different lines
+xaxis_var = pd.Series(data=["Points1", "Points0"], index=line_loc_suffixes)
+xaxis_labels = pd.Series(data=[r"$y$", r"$x$"], index=line_loc_suffixes)
+
+N_colors = pd.Series(data=["blue", "green", "red", "magenta"], index=N_values)
+N_linestyles = pd.Series(
+    data=[
+        (0, ()), # solid line
+        (0, (1,1)), # dotted
+        (0, (5,1)), # dense dash
+        (0, (5,2,1,2)) # dash dot
+    ],
+    index=N_values
+)
+for line_suffix in line_loc_suffixes:
+    fig, ax = plt.subplots(1,1)
+    for N in N_values:
+        data = np.genfromtxt(data_filenames.loc[N, line_suffix], delimiter=",", names=True)
+        ax.plot(
+            data[xaxis_var.loc[line_suffix]],
+            data[plot_var],
+            label=r"$N={}$".format(N),
+            ls=N_linestyles.loc[N],
+            marker="",
+            c=N_colors.loc[N],
+            alpha=0.75
+        )
+    ax.set_xlabel(xaxis_labels.loc[line_suffix])
+    ax.set_ylabel(yaxis_label)
+    ax.grid()
+    ax.legend()
+    fig.tight_layout()
+    plt.show()
+    for fmt in ["png", "pdf"]:
+        fig_filename = "{}/line_{}.{}".format(plot_dir, line_suffix, fmt)
+        fig.savefig(fig_filename, format=fmt)
+        print("Saved file {}".format(fig_filename))
