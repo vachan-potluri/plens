Bug fix in blender calculation
This was probably causing the difference in simulation results between modified fluxo and plens, see WJ notes around 11-Nov-2022

study2/hcef_run9/scripts/extract_data.py
@@ -1,6 +1,7 @@
 # A script to extract surface data from pvtu files for HCEF geometry
 
 import argparse
+import numpy as np
 from paraview.simple import *
 
 parser = argparse.ArgumentParser(
@@ -24,7 +25,58 @@ parser.add_argument(
     default=100,
     action="store"
 )
+parser.add_argument(
+    "-a",
+    "--azimuths",
+    help="Extract data on multiple azimuthal angles (in degrees). Default: ''. If this argument is "
+        + "empty, only the zero-degree azimuthal angle is used for extracting.",
+    default=[0.0],
+    type=float,
+    action="store",
+    nargs="*"
+)
 args = parser.parse_args()
+
+
+
+# function to save cylinder and flare data on a given azimuthal angle
+def extract_data(result, res_dir, counter, azimuth=0):
+    phi = np.pi*azimuth/180
+    eps = 1e-6
+    r1 = 0.0325 + eps
+    r2 = 0.1008 + eps
+    x1 = 0.01
+    x2 = 0.1017
+    x3 = 0.22
+    plotOverLine1 = PlotOverLine(
+        registrationName='PlotOverLine1',
+        Input=result,
+    )
+    plotOverLine1.Point1 = [x1, r1*np.cos(phi), r1*np.sin(phi)]
+    plotOverLine1.Point2 = [x2, r1*np.cos(phi), r1*np.sin(phi)]
+    plotOverLine1.Resolution = args.resolution
+    temp_filename = '{}cylinder_data_phi{:.0f}_{}.csv'.format(res_dir, azimuth, counter)
+    SaveData(
+        temp_filename,
+        proxy=plotOverLine1,
+    )
+    print("Written file {}".format(temp_filename))
+
+    plotOverLine2 = PlotOverLine(
+        registrationName='PlotOverLine2',
+        Input=result,
+    )
+    plotOverLine2.Point1 = plotOverLine1.Point2
+    plotOverLine2.Point2 = [x3, r2*np.cos(phi), r2*np.sin(phi)]
+    plotOverLine1.Resolution = args.resolution
+    temp_filename = '{}flare_data_phi{:.0f}_{}.csv'.format(res_dir, azimuth, counter)
+    SaveData(
+        temp_filename,
+        proxy=plotOverLine2,
+    )
+    print("Written file {}".format(temp_filename))
+
+
 
 res_dir = args.res_dir
 if res_dir[-1] != "/":
@@ -36,43 +88,4 @@ result = XMLPartitionedUnstructuredGridReader(
 )
 print("Read file {}".format(full_data_filename))
 
-# create a new 'Plot Over Line'
-plotOverLine1 = PlotOverLine(
-    registrationName='PlotOverLine1',
-    Input=result,
-    Source='Line'
-)
-
-plotOverLine1.Source.Point1 = [0.01, 0.0325, 1e-8]
-plotOverLine1.Source.Point2 = [0.1017, 0.0325, 1e-8]
-plotOverLine1.Source.Resolution = args.resolution
-
-# save data
-temp_filename = '{}cylinder_data_{}.csv'.format(res_dir, counter)
-SaveData(
-    temp_filename,
-    proxy=plotOverLine1,
-    PointDataArrays=['Subdomain', 'T', 'alpha', 'arc_length', 'k', 'mu', 'p', 'qx', 'qy', 'qz', 'rho', 'rhoE', 'rhou', 'rhov', 'rhow', 'steady_state_error', 'txx', 'txy', 'txz', 'tyy', 'tyz', 'tzz', 'u', 'v', 'vtkValidPointMask', 'w']
-)
-print("Written file {}".format(temp_filename))
-
-# create a new 'Plot Over Line'
-plotOverLine2 = PlotOverLine(
-    registrationName='PlotOverLine2',
-    Input=result,
-    Source='Line'
-)
-
-plotOverLine2.Source.Point1 = [0.1017, 0.0325, 1e-8]
-plotOverLine2.Source.Point2 = [0.22, 0.100801, 1e-8]
-plotOverLine1.Source.Resolution = args.resolution
-
-# save data
-temp_filename = '{}flare_data_{}.csv'.format(res_dir, counter)
-SaveData(
-    temp_filename,
-    proxy=plotOverLine2,
-    PointDataArrays=['Subdomain', 'T', 'alpha', 'arc_length', 'k', 'mu', 'p', 'qx', 'qy', 'qz', 'rho', 'rhoE', 'rhou', 'rhov', 'rhow', 'steady_state_error', 'txx', 'txy', 'txz', 'tyy', 'tyz', 'tzz', 'u', 'v', 'vtkValidPointMask', 'w']
-)
-print("Written file {}".format(temp_filename))
-
+for azimuth in args.azimuths: extract_data(result, res_dir, counter, azimuth)

study2/hcef_run9/scripts/plot.py
@@ -19,70 +19,85 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("res_dir", help="Result directory (absolute or relative).")
 parser.add_argument("counter", help="Output counter.", type=int)
+parser.add_argument(
+    "-a",
+    "--azimuths",
+    help="Extract data on multiple azimuthal angles (in degrees). Default: ''. If this argument is "
+        + "empty, only the zero-degree azimuthal angle is used for extracting.",
+    default=[0.0],
+    type=float,
+    action="store",
+    nargs="*"
+)
 args = parser.parse_args()
 
 res_dir = args.res_dir
 if res_dir[-1] != "/":
     res_dir += "/"
 counter = args.counter
-cylinder_data = np.genfromtxt(
-    "{}cylinder_data_{}.csv".format(res_dir, counter), delimiter=",", names=True
-)
-flare_data = np.genfromtxt(
-    "{}flare_data_{}.csv".format(res_dir, counter), delimiter=",", names=True
-)
 harvey_cp = np.genfromtxt("../data/cp_harvey.csv", delimiter=",")
 gnoffo_cp = np.genfromtxt("../data/cp_gnoffo.csv", delimiter=",")
 harvey_St = np.genfromtxt("../data/St_harvey.csv", delimiter=",")
 gnoffo_St = np.genfromtxt("../data/St_gnoffo.csv", delimiter=",")
 
-x_cylinder = cylinder_data["Points0"]/L
-x_flare = flare_data["Points0"]/L
-x = np.concatenate([x_cylinder, x_flare])
+fig1, ax1 = plt.subplots(1,1)
+fig2, ax2 = plt.subplots(1,1)
+for azimuth in args.azimuths:
+    phi = np.pi*azimuth/180
+    cylinder_data = np.genfromtxt(
+        "{}cylinder_data_phi{:.0f}_{}.csv".format(res_dir, azimuth, counter),
+        delimiter=",",
+        names=True
+    )
+    flare_data = np.genfromtxt(
+        "{}flare_data_phi{:.0f}_{}.csv".format(res_dir, azimuth, counter),
+        delimiter=",",
+        names=True
+    )
 
-cp_cylinder = 2*(cylinder_data["p"] - p_inf)/(rho_inf*u_inf**2)
-cp_flare = 2*(flare_data["p"] - p_inf)/(rho_inf*u_inf**2)
-cp = np.concatenate([cp_cylinder, cp_flare])
-cp_mask = np.isfinite(cp)
+    x_cylinder = cylinder_data["Points0"]/L
+    x_flare = flare_data["Points0"]/L
+    x = np.concatenate([x_cylinder, x_flare])
 
-St_cylinder = -2*cylinder_data["qy"]/(rho_inf*u_inf**3)
-St_flare = -2*(flare_data["qy"]*np.cos(np.pi/6) - flare_data["qx"]*np.sin(np.pi/6))/(rho_inf*u_inf**3)
-St = np.concatenate([St_cylinder, St_flare])
-St_mask = np.isfinite(St)
+    cp_cylinder = 2*(cylinder_data["p"] - p_inf)/(rho_inf*u_inf**2)
+    cp_flare = 2*(flare_data["p"] - p_inf)/(rho_inf*u_inf**2)
+    cp = np.concatenate([cp_cylinder, cp_flare])
+    cp_mask = np.isfinite(cp)
+    ax1.plot(x[cp_mask], cp[cp_mask], ls="--", label=r"$\phi={:.0f}$".format(azimuth), alpha=0.5)
 
-txy_cylinder = cylinder_data["txy"]
-txy_flare = flare_data["txy"]
-txy = np.concatenate([txy_cylinder, txy_flare])
-txy_mask = np.isfinite(txy)
 
-fig, ax = plt.subplots(1,1)
-ax.plot(harvey_cp[:,0], harvey_cp[:,1], "bo", label="Harvey et al (2001)\nexperiment")
-ax.plot(gnoffo_cp[:,0], gnoffo_cp[:,1], "g^", label="Gnoffo (2001)\nsimulation")
-ax.plot(x[cp_mask], cp[cp_mask], "r-", label="PLENS")
-ax.set_xlabel(r"$x/L$")
-ax.set_ylabel(r"$\displaystyle\frac{p-p_\infty}{\frac{1}{2}\rho_\infty u_\infty^2}$", rotation=0, labelpad=25)
-ax.grid()
-ax.legend(loc="best")
-fig.tight_layout(pad=0.25)
+
+    St_cylinder = -2*cylinder_data["qy"]/(rho_inf*u_inf**3)
+    St_flare = -2*(
+        (np.cos(phi)*flare_data["qy"] + np.sin(phi)*flare_data["qz"])*np.cos(np.pi/6) -
+        flare_data["qx"]*np.sin(np.pi/6)
+    )/(rho_inf*u_inf**3)
+    St = np.concatenate([St_cylinder, St_flare])
+    St_mask = np.isfinite(St)
+    ax2.plot(x[St_mask], St[St_mask], ls="--", label=r"$\phi={:.0f}$".format(azimuth), alpha=0.5)
+
+ax1.plot(harvey_cp[:,0], harvey_cp[:,1], "bo", label="Harvey et al (2001)\nexperiment")
+ax1.plot(gnoffo_cp[:,0], gnoffo_cp[:,1], "g^", label="Gnoffo (2001)\nsimulation")
+ax1.set_xlabel(r"$x/L$")
+ax1.set_ylabel(r"$\displaystyle\frac{p-p_\infty}{\frac{1}{2}\rho_\infty u_\infty^2}$", rotation=0, labelpad=25)
+ax1.grid()
+ax1.legend(loc="best")
+fig1.tight_layout(pad=0.25)
 for fmt in ["png", "pdf"]:
     full_figname = "{}cp_comparison_{}.{}".format(res_dir, counter, fmt)
-    fig.savefig(full_figname, format=fmt)
+    fig1.savefig(full_figname, format=fmt)
     print("Written file {}".format(full_figname))
-plt.show()
 
-del fig, ax
-fig, ax = plt.subplots(1,1)
-ax.plot(harvey_St[:,0], harvey_St[:,1], "bo", label="Harvey et al (2001)\nexperiment")
-ax.plot(gnoffo_St[:,0], gnoffo_St[:,1], "g^", label="Gnoffo (2001)\nsimulation")
-ax.plot(x[St_mask], St[St_mask], "r-", label="PLENS")
-ax.set_xlabel(r"$x/L$")
-ax.set_ylabel(r"$\displaystyle\frac{2q^{\prime\prime}_w}{\rho_\infty u_\infty^3}$", rotation=0, labelpad=20)
-ax.grid()
-ax.legend(loc="best")
-fig.tight_layout(pad=0.25)
+ax2.plot(harvey_St[:,0], harvey_St[:,1], "bo", label="Harvey et al (2001)\nexperiment")
+ax2.plot(gnoffo_St[:,0], gnoffo_St[:,1], "g^", label="Gnoffo (2001)\nsimulation")
+ax2.set_xlabel(r"$x/L$")
+ax2.set_ylabel(r"$\displaystyle\frac{2q^{\prime\prime}_w}{\rho_\infty u_\infty^3}$", rotation=0, labelpad=20)
+ax2.grid()
+ax2.legend(loc="best")
+fig2.tight_layout(pad=0.25)
 for fmt in ["png", "pdf"]:
     full_figname = "{}St_comparison_{}.{}".format(res_dir, counter, fmt)
-    fig.savefig(full_figname, format=fmt)
+    fig2.savefig(full_figname, format=fmt)
     print("Written file {}".format(full_figname))
 plt.show()
 